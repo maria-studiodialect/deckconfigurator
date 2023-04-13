82687ef3e057a324292a36095680ccf8dc1663dc
maria-studiodialect <maria@studiodialect.com>
2023-04-13 16:49:35 +0100

    updates

--- components/YourDetails.js
+++ components/YourDetails.js
@@ -1,12 +1,17 @@
 import Link from "next/link"
+import { useEffect, useState } from 'react'
 
 const YourDetails = ({text, mainColour, href, icon}) => {
-    const formData = JSON.parse(sessionStorage.getItem('formData') || '{}');
-
+    const [formData, setFormData] = useState(null)
+    useEffect(() => {
+        const storedData = JSON.parse(sessionStorage.getItem('formData') || '{}');
+        setFormData(storedData)
+    })
     return (
         <div className="bg-beige rounded-xl p-9 w-1/4 shadow-xl">
             <div>YOUR DETAILS</div>
             <div className="mt-3 text-sm">Lorem ipsum dolor sit amet dictum consectetur adipiscing elit. Etiameu turpis molestie.</div>
+            {formData &&
             <div className="grid grid-cols-2 gap-3 mt-7 border-y py-7 text-sm">
                 <div>NAME</div>
                 <div className="text-end">{formData.name}</div>
@@ -19,6 +24,7 @@ const YourDetails = ({text, mainColour, href, icon}) => {
                 <div>SKIN TONE</div>
                 <div className="rounded-full w-5 h-5 justify-self-end" style={{backgroundColor: formData.skinTone}}></div>
             </div>
+            }
         </div>
     )
 }
